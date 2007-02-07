8b61511926e96b00200783bc81c708f00c23bae5
brandon <brandon@fda8ee82-3e3d-0410-b8ac-e166fe2dbcef>
2007-02-07 04:39:04 +0000

    Added rudimentary motion

--- handlers/interaction.py
+++ handlers/interaction.py
@@ -2,10 +2,10 @@ from libsigma import *
 
 def register_handlers():
 	return	{
-		 "say" : say,
-		 "emote" : emote,
-		 "look" : look,
-		 "go" : go
+		"say" : say,
+		"emote" : emote,
+		"look" : look,
+		"go" : go
 		}
 
 def say(data):
@@ -17,14 +17,28 @@ def emote(data):
 def look(data):
 	speaker = data["speaker"]
 
-	import world
 	speaker.send_line(speaker.location.name)
 	speaker.send_line(speaker.location.desc)
-	
+
 	speaker.send("Exits: ")
 	for dir in exits(speaker.location):
 		speaker.send(dir2txt(dir) + " ")
 	speaker.send_line("")
 
 def go(data):
-	log("GO", "GO")
+	speaker = data["speaker"]
+	args = data["args"]
+
+	dir = -1
+
+	if "go".startswith(args[0]) and len(args) == 2:
+		dir = txt2dir(args[1])
+	elif len(args) == 1:
+		dir = txt2dir(args[0])
+
+	if dir == -1:
+		speaker.send_line("Where do you want to go?")
+	elif speaker.location.exits[dir]:
+		enter_room(speaker, speaker.location.exits[dir])
+	else:
+		speaker.send_line("There is no exit in that direction.")

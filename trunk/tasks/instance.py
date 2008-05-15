8d9aa7fd4243c281df31f2c2e875e88bcb814c8b
bmccalip <bmccalip@fda8ee82-3e3d-0410-b8ac-e166fe2dbcef>
2008-05-15 05:28:52 +0000

    Minor clean-up of metadata and program data

--- trunk/tasks/instance.py
+++ trunk/tasks/instance.py
@@ -3,7 +3,7 @@ import world
 from common import *
 
 def task_info():
-	return ('Instance Denizens and Items', 'Sigma Internal', "1.0", 100)
+	return ('Instance Denizens and Items', 'Sigma Internal', '1.0', 100)
 
 def task_init():
 	task_execute()

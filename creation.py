a70da48872c27103aaec80d4e46e60b86c21a289
bmccalip <bmccalip@fda8ee82-3e3d-0410-b8ac-e166fe2dbcef>
2008-08-15 23:02:35 +0000

    Reorganization and re-stubbing of player classes.
    1. No more use of "orders", just "classes" and "character_class"
    2. Module creation will contain races and classes
    3. Classes and races will be handled as XML nodes rather than Python modules

---
+++ creation.py
@@ -0,0 +1,12 @@
+## @package creation
+#  Defines the character class and race framework.
+
+## Holds all available character classes
+classes = {}
+
+## Holds all available character races
+races = {}
+
+class character_class(object):
+	def __init__(self, node):
+		pass

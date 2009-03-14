2ecc19da7cd174d1fe12fc93d55ac8262aeddea6
bmccalip <bmccalip@fda8ee82-3e3d-0410-b8ac-e166fe2dbcef>
2009-03-14 07:38:21 +0000

    Time handler and hashlib updates
    Updated the time handler to pad minutes and hours with a leading zero when necessary.  Fixes #9
    
    Switched password digest to hashlib rather than deprecated (Python 2.6) sha library.

--- handlers/global.py
+++ handlers/global.py
@@ -1,3 +1,5 @@
+import string
+
 from libsigma import *
 from world import *
 from common import *
@@ -7,7 +9,7 @@ def time(data):
     speaker = data["speaker"]
     date_time = calendars[0].get_current_IG_DateTime()
     speaker.send_line( "It is "+ date_time["day_of_week"] +", the " + ordinals(date_time["day"]) + " of " + str(date_time["month"]) + ", " + str(date_time["year"]) + " years since the " + calendars[0].watershed_name + "." )
-    speaker.send_line("It is " + str(date_time["hour"]) + ":" + str(date_time["minute"]) + ".")
+    speaker.send_line("It is " + string.zfill(date_time["hour"], 2) + ":" + string.zfill(date_time["minute"], 2) + ".")
 
 @handler
 def statistics(data):
